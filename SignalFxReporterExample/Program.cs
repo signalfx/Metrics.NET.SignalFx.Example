cf436a4ab2c7caa2959e373a9c72295c59a55425
Ted Crossman <tedo@signalfuse.com>
2017-09-07 09:23:25 -0700

    Initial commit

--- SignalFxReporterExample/Program.cs
+++ SignalFxReporterExample/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Metrics;
+using Metrics.Core;
+using System.Threading;
 
 namespace SignalFxReporterExample
 {
     class Program
     {
+        private static TaggedMetricsContext context;
+        private static Counter myCounter;
+        private static Metrics.Timer myTimer;
+
         static void Main(string[] args)
         {
+            InitializeSignalFx("SignalFxReporterExample");
+            CollectData();
+        }
+
+        static void InitializeSignalFx(string contextName)
+        {
+            Metric.Config.WithReporting(Report => Report.WithSignalFxFromAppConfig());
+            context = (TaggedMetricsContext)Metric.Context(contextName, (ctxName) => { return new TaggedMetricsContext(ctxName); });
+            // Create a counter that has the dimension "api:login"
+            myCounter = context.Counter("myCounter", Unit.Calls, tags: new MetricTags("api", "login"));
+            // Create a timer
+            myTimer = context.Timer("myTimer", Unit.Calls);
+        }
+
+        static void CollectData()
+        {
+            while (true)
+            {
+                myCounter.Increment();
+                using (myTimer.NewContext())
+                {
+                    Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
